a6c0422efdf0c545ce9f421802408676cdfc02b8
unknown <sharma.803061@gmail.comProfile image>
2025-08-29 18:38:49 +0600

    added a seven file

---
+++ js/script.js
@@ -0,0 +1,35 @@
+document.getElementById("national-number").addEventListener("click", function() {
+    document.getElementById("Emergency-number").style.display = "block"
+})
+
+document.getElementById("Police-Help").addEventListener("click", function() {
+    document.getElementById("helpline-number").style.display = "block"
+})
+
+document.getElementById("fire-service").addEventListener("click", function() {
+    document.getElementById("Service-number").style.display = "block"
+})
+
+document.getElementById("Ambulance-Service").addEventListener("click", function() {
+    document.getElementById("Ambulance").style.display = "block"
+})
+
+document.getElementById("Women-Helpline").addEventListener("click", function() {
+    document.getElementById("Women").style.display = "block"
+})
+
+document.getElementById("Anti-Helpline").addEventListener("click", function() {
+    document.getElementById("Anti").style.display = "block"
+})
+
+document.getElementById("Electricity-helpline").addEventListener("click", function() {
+    document.getElementById("Electricity").style.display = "block"
+})
+
+document.getElementById("brac-helpline").addEventListener("click", function() {
+    document.getElementById("brac").style.display = "block"
+})
+
+document.getElementById("Railway-helpline").addEventListener("click", function() {
+    document.getElementById("Railway").style.display = "block"
+})
